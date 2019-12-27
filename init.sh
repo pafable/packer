b208cc121e602095119f49c2e9d350f35344554b
Philip Afable <philip.afable@US-GN-43788.local>
2019-12-27 09:31:35 -0500

    added new dir test

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo yum update -y && yum upgrade -y
 sudo yum install -y wget amazon-efs-utils
 sudo mkdir /efs
 
-sudo useradd --no-create-home --shell /bin/false node-exporter-user
-sudo wget -P /usr/bin https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
-sudo tar -xvf /usr/bin/node_exporter-0.18.1.linux-amd64.tar.gz
-sudo mv /tmp/node-exporter.service /etc/systemd/system
+#sudo useradd --no-create-home --shell /bin/false node-exporter-user
+#sudo wget -P /usr/bin https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+#sudo tar -xvf /usr/bin/node_exporter-0.18.1.linux-amd64.tar.gz
+#sudo mv /tmp/node-exporter.service /etc/systemd/system
